26913c941613b4b94dbf314e154510f813d146e4
Giovanni Mendoza <mendozagioo@gmail.com>
2013-03-17 14:53:14 -0600

    Change all repository to Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cactu::Application.config.secret_key_base = 'b24b811bea86ffc8c359759d6b574a62b147bda93f3cd266c6ddc2efabaff9ae9641c8ffca69066d81f5cced8ded7927bf8fff985cc90a75e00d5be3f7bffa21'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Cactu::Application.routes.draw do
+  root to: 'home#index'
+end
